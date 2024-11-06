af4a645b47356d74cbdce05bfea55b9d3e8ce6df
BisalDangol <message2bishal@outlook.com>
2024-11-06 07:53:08 +0545

    middleware module

---
+++ middleware/auth.mw.js
@@ -0,0 +1,26 @@
+/**
+ * validating the user data in signup
+ */
+
+const verifyUserSignUpBody = (req, res, next) => {
+  try {
+    //user name varifying
+    if (!req.body.name) {
+      return res.status(400).json({ message: "Name is required" });
+    }
+    //user email varifying
+    if (!req.body.email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+    //user  userid varifying
+    if (!res.body.userId){
+        return res.status(400).json({message:"userId is required"})
+    }
+    //userid is already or not
+  } catch (error) {
+    console.log("something wents wrong");
+    res.status(500).send({
+      message: "Internal Server Error",
+    });
+  }
+};
